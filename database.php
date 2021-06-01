550f1e65ba671745013b9d766158e0b1966d5c31
Bec <70154628+BecWy@users.noreply.github.com>
2021-06-01 15:13:32 +0100

    Updated latest-posts.php
    Displaying post data from the database

--- database.php
+++ database.php
@@ -7,7 +7,7 @@ try {
   $db = new PDO("mysql:host=$servername;dbname=php-reflection", $username, $password);
   // set the PDO error mode to exception
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  echo "Connected successfully";
+  //echo "Connected successfully";
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
--- latest-posts.php
+++ latest-posts.php
@@ -6,7 +6,7 @@ $query = 'SELECT * FROM posts ORDER BY posted_date DESC LIMIT 3';
 try {
 $results = $db->query($query); 
 $results->execute();
-echo 'I have the data';
+//echo 'I have the data';
 } catch(Exception $e) {
     echo $e->getMessage();
     die();
@@ -27,9 +27,11 @@ $posts = $results->fetchAll(PDO::FETCH_ASSOC);
     <h2><span id="latest-title">Latest</span></h2>  
     <div class="news-container">
 
-    <?php foreach($posts as $post) { ?>
-
-        <div class="news news-1">
+    <?php 
+    $postNum = 1;
+    foreach($posts as $post) { 
+    ?>
+        <div class="news <?php echo "news-" . $postNum ?>"> <!-- creates the classes news-1 / news-2 / news-3 -->
             <div class="tooltip">
                 <a href="#"><h3><?php echo $post["category"] ?></h3></a>
                 <div class="tooltip-text">
@@ -38,31 +40,35 @@ $posts = $results->fetchAll(PDO::FETCH_ASSOC);
             </div>                     
             <a href="#">
                 <div class="news-image-parent">
-                    <div class="news-1-image-child news-image-child">
+                    <div class="news-image-child <?php echo "news-" . $postNum . "-image-child" ?>" > <!-- creates the classes news-1-image-child / news-2-image-child / news-3-image-child -->
                     </div>
                 </div>
             </a> <!-- larger screens - background image -->
             <a href="#">
-                <img src="<?php echo $post["img_src"] ?>" alt="Ecommerce website advert">
+                <img src="<?php echo $post["img_src"] ?>" alt="<?php echo $post["title"] ?>">
             </a> <!-- small screens - inserted image -->
-            <div class="news-text news-text-1">                
+            <div class="news-text <?php echo "news-text-" . $postNum ?>">  <!-- creates the classes news-text-1 / news--ext-2 / news-text-3 -->              
                 <h3><a href="#"><?php echo substr($post["title"], 0, 44) . '...' ?></a></h3>
                 <p><?php echo $post["post_content_snippet"] ?>...</p>
                 <div class="button"><a href="#">Read More</a></div>
                 <div class="posted-by">
-                    <div class="thumbnail"><img src="./images/thumbnails/thumbnail-1.jpeg" alt="Photo of James Gulliver"></div>
+                    <div class="thumbnail"><img src="<?php echo $post["posted_by_img_src"] ?>" alt="Photo of <?php echo $post["posted_by"] ?>"></div>
                     <div class="posted-by-text">
-                        <p><strong> Posted by James Gulliver</strong></p>
-                        <p>26th October 2020</p>                                
+                        <p><strong> Posted by <?php echo $post["posted_by"] ?></strong></p>
+                        <p><?php echo $post["posted_date"] ?></p>                                
                     </div>    
                 </div>
             </div>    
         </div>     
     
-    <?php  } ?>
+    <?php  
+    $postNum++;
+    } ?>
         
     </div> <!--end news-container-->
 </div> <!-- end latest -->
 
 
 
+<!-- 
+style="background-image: url('<//?php echo $post["img_src"] ?>');" -->
